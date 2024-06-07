681198e83f6d28986f2f9707be1793748892e23d
fauzanalrafii <fauzanalrafi16@gmail.com>
2024-06-07 14:31:16 +0700

    updated fetchnutrients for query sql

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: '34.34.221.182',
-    user: 'root',
-    password: 'capstonenutrilog',
-    database: 'Nutrilog-db'
+    host: '',
+    user: '',
+    password: '',
+    database: ''
 });
 
 module.exports = pool;
